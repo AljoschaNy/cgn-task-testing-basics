a87a71a0f304cccaf061401a41a43beb3d18fb50
third version <soheila.mohammadi@web.de>
2023-09-20 16:09:05 +0200

    add texttouppercase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,11 +21,18 @@ public class Main {
     public static int multiply(int number1, int number2) {
         return number1 * number2;
     }
-}
-// Schreibe einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
-// Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
-}
 
+    // Schreibe einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
+    public static String textToUpperCase(String letter) {
+        return letter.toUpperCase();
+
+
+    }
+
+
+// Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
+
+}
 
 // Schreibe einen Test für eine Methode, die die Summe von zwei ganzen Zahlen berechnet.
 // Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl gerade ist (even).
