80fe2fc1d2ef9aa048ea3c18ac1288f1ddd1c22a
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-20 15:43:25 +0200

    Add sum method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
+
+    // Schreibe einen Test für eine Methode, die die Summe von zwei ganzen Zahlen berechnet.
+    public static int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    // Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl gerade ist (even).
+    // Schreibe einen Test für eine Methode, die das Produkt zweier ganzer Zahlen berechnet.
+    // Schreibe einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
+    // Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
 }
+
+
+
+
+
+
+// Schreibe einen Test für eine Methode, die die Summe von zwei ganzen Zahlen berechnet.
+// Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl gerade ist (even).
+// Schreibe einen Test für eine Methode, die das Produkt zweier ganzer Zahlen berechnet.
+// Schreibe einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
+// Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
