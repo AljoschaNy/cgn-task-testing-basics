d57aafb6cfece551c5becf7c33cf427bbaa5a9a2
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-20 16:25:11 +0200

    Add test for sum method

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -6,22 +6,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    // Schreibe einen Test für eine Methode, die die Summe von zwei ganzen Zahlen berechnet.
     @Test
     void sum() {
+        //GIVEN
+        int number1 = 10;
+        int number2 = 20;
+        int expect = number1 + number2;
+        //WHEN
+        int actual = Main.sum(number1,number2);
+        //THEN
+        assertEquals(expect,actual);
     }
 
+    // Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl gerade ist (even).
     @Test
     void isEven() {
     }
 
+    // Schreibe einen Test für eine Methode, die das Produkt zweier ganzer Zahlen berechnet.
     @Test
     void multiply() {
     }
 
+    // Schreibe einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
     @Test
     void textToUpperCase() {
     }
 
+    // Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
     @Test
     void isNumberPositive() {
     }
